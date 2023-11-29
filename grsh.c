ef3a1b3b4cc5c9dd7b7f1bd143d70736208bb2e2
JohnPluznyk <115428211+JohnPluznyk@users.noreply.github.com>
2023-11-29 15:30:05 -0500

    Add files via upload

--- grsh.c
+++ grsh.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <stdint.h>
+#include <unistd.h>
 #include <string.h>
 
 //what is a shell
@@ -45,7 +45,8 @@ int main(int argc, char *argv[]){
             if(dup[i] == ' ')  //if we dont find white space add argument to an array of strings  //do i need to find how many spaces are first?
                 num_args++;
         }
-        num_args++;  //allocate one more space for num_args
+        num_args++;  //allocate one more space for the last word
+        num_args++;  //allocate one more space for NULL
         printf("num_args: %d\n\n", (num_args));
         //now I need to create an array to store all my arguments like argv
 
@@ -54,39 +55,39 @@ int main(int argc, char *argv[]){
         char *arguments[num_args];
         arguments[0] = strtok(dup, " ");  //I should see if I can store it in an aray of pointers instead of just one        
         
-        for (int i = 1; i < num_args; i++)
+        for (int i = 1; i < num_args - 1; i++)  //fill array with arguments
             arguments[i] = strtok(NULL, " ");
         
-        
+        arguments[num_args - 1] = NULL;  //set last arguemnt to NULL
 
         //this below for loops simpleu just prints out the value
         
-        for(int i = 0; i<num_args; i++){
+        for(int i = 0; i<num_args; i++){  //print out arguments
             printf("args[%d]: %s\n", i, arguments[i]);
         }
         
-        num_args = 0;
+
+        //fork();//we want to fork the process and call execute
+        //exec();
+
+        pid_t p = fork();
+
+        if(p<0){
+            perror("fork fail");
+            exit(1);
+        }
+
+        else if (p == 0){
+            execvp(arguments[0], arguments);
+        }
+        sleep(1);
+        printf("\n");
+        printf("Hello World! process_id(%d)\n", getpid());
         
         //free allocated memory
         //free(dup);
+        num_args = 0;
     }
 
     return 0;
 }
-
-/*
-    if argument count is 2
-        go into batch mode
-        read file
-        while file not end
-            pass line to function
-    else if argument count is 1
-        go into interactive mode
-        do
-            print prompt
-            read line from user
-            pass line to function
-        while (true)
-    else
-        error message 
-*/
